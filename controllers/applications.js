f6328aceb842abdb0a694cc370ffc7af668eb9c8
Ben Manning <bmanning@me.com>
2024-06-26 15:10:23 -0400

    create applications controller

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-
+router.get('/', (req, res) => {
+  try {
+    res.render('applications/index.ejs');
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
 
 module.exports = router;
