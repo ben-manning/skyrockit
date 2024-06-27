f3389a7786126a0d72b5c35b51b9176069ce6bf4
Ben Manning <bmanning@me.com>
2024-06-27 10:50:28 -0400

    add show page for application

--- controllers/applications.js
+++ controllers/applications.js
@@ -34,4 +34,20 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.get('/:applicationId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+
+    const application = currentUser.applications.id(req.params.applicationId)
+
+    res.render('applications/show.ejs', {
+      application: application,
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 module.exports = router;
