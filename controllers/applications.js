fb1b41710fc9039ee0c8fd7bf4cd942cc9451820
Ben Manning <bmanning@me.com>
2024-06-27 11:50:01 -0400

    complete update functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -78,4 +78,22 @@ router.delete('/:applicationId', async (req, res) => {
   }
 });
 
+router.put('/:applicationId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+
+    application.set(req.body);
+
+    await currentUser.save();
+
+    res.redirect(
+      `/users/${currentUser._id}/applications/${req.params.applicationId}`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 module.exports = router;
