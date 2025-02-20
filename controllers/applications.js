505d5fc1f7e7c4ac2bd46fd33cfe4a00c1d3dd8e
Ben Manning <bmanning@me.com>
2025-02-20 12:51:34 -0500

    add POST route for applications

--- controllers/applications.js
+++ controllers/applications.js
@@ -20,6 +20,23 @@ router.get('/new', async (req, res) => {
   res.render('applications/new.ejs');
 });
 
+// POST /users/:userId/applications
+router.post('/', async (req, res) => {
+  try {
+    // E
+    const currentUser = await User.findById(req.session.user._id);
+    // T
+    currentUser.applications.push(req.body);
+    // L
+    await currentUser.save();
+
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+  
+});
 
 
 
