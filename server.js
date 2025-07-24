3d21388424adbb0ed0598c98f8254ac82a34373f
Ben Manning <bmanning@me.com>
2025-07-24 08:24:25 -0700

    complete applications index route and view template

--- server.js
+++ server.js
@@ -38,9 +38,11 @@ app.use(session({
 app.use(passUserToView); // pass that user information to an EJS template
 
 app.get('/', async (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user
-  });
+  if (req.session.user) {
+    res.redirect(`/users/${req.session.user._id}/applications`);
+  } else {
+    res.render('index.ejs');
+  }
 });
 
 app.use('/auth', authController); // these are the auth routes
